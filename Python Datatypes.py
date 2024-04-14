f4def1a5c9725aedb439c610a7c521d2ac89dde2
Subhashree-ayanshi <143612383+Subhashree-ayanshi@users.noreply.github.com>
2024-04-14 11:10:47 +0530

    Add files via upload

---
+++ Python Datatypes.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+# Defining List
+l = [23,34,67,89,38]
+l
+
+
+# In[12]:
+
+
+# Appending a List
+l.append(78)
+l
+
+
+# In[13]:
+
+
+# Inserting an element in specific position
+# method : insert(index , value)
+l.insert(2,65)
+l
+
+
+
+# In[14]:
+
+
+l = [23, 34, 65, 67, 89, 38, 78,34]
+# Removing an element from the list
+l.remove(67)
+l
+
+
+# In[15]:
+
+
+l.remove(34)
+l
+
+
+# In[16]:
+
+
+# Modifying an element in a list
+l[4] = 'Jenny'
+l
+
+
+# In[1]:
+
+
+# Defining a dictionary
+dict = {
+    'Name' : 'Jenny',
+    'Age' : 22,
+    'Weight':56
+}
+dict
+
+
+# In[2]:
+
+
+# Accessing A dictionary
+dict['Name']
+
+
+# In[3]:
+
+
+# Adding new value to the dictionary
+dict['City']='Bhubaneswar'
+
+
+# In[4]:
+
+
+dict
+
+
+# In[5]:
+
+
+# Deleting a value from dictionary
+dict.pop('Weight')
+dict
+
+
+# In[6]:
+
+
+# Modifying the dictionary
+dict['Age'] = 23
+dict
+
+
+# In[7]:
+
+
+# Defining a set
+s = {1,2,3,4,5}
+s
+
+
+# In[8]:
+
+
+# Adding an item in the set
+s.add(7)
+s
+
+
+# In[9]:
+
+
+# Removing an element from a set
+s.remove(7)
+s
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
